56fb872f57c2dfbc619966177f2b3f2adcb84a1a
Likun Cai <kentclk@outlook.com>
2026-04-22 18:44:47 -0400

    feat: integrate sorting and filtering in feed provider

--- lib/features/feed/feed_provider.dart
+++ lib/features/feed/feed_provider.dart
@@ -1,10 +1,27 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../core/client/twitter_client.dart';
 import '../../core/models/tweet.dart';
+import '../settings/settings_provider.dart';
 
 final twitterClientProvider = Provider((ref) => TwitterClient());
 
 final feedProvider = FutureProvider<List<Tweet>>((ref) async {
   final client = ref.watch(twitterClientProvider);
-  return client.fetchTrendingMedia();
+  final settings = ref.watch(settingsProvider);
+  
+  var tweets = await client.fetchTrendingMedia();
+
+  // Apply Filter
+  if (settings.filter == MediaFilter.videoOnly) {
+    tweets = tweets.where((t) => t.isVideo).toList();
+  } else if (settings.filter == MediaFilter.imageOnly) {
+    tweets = tweets.where((t) => !t.isVideo).toList();
+  }
+
+  // Apply Sort (Mock sorting for now)
+  if (settings.sort == FeedSort.oldest) {
+    tweets = tweets.reversed.toList();
+  }
+
+  return tweets;
 });
