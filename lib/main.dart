47311aa3e54b6ee1e09db24e8936626c7b9bd0d2
Likun Cai <kentclk@outlook.com>
2026-04-22 18:43:38 -0400

    feat: initialize media_kit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:media_kit/media_kit.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  MediaKit.ensureInitialized();
   runApp(const ProviderScope(child: XFlowApp()));
 }
 
